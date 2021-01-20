c9e96ada40fffdfde6d05b01089c80eda713fa1c
Fabio <offabio@outlook.com>
2021-01-20 11:19:40 +0100

    chore: updated app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,15 +3,14 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import BeamsWrapper from 'react-native-beams-wrapper';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    // BeamsWrapper.multiply(3, 7).then(setResult);
-    BeamsWrapper.on('notification', function(payload) {
-      console.log(payload);
-    });
+    BeamsWrapper.multiply(3, 7).then(setResult);
+    // BeamsWrapper.on('notification', function(payload) {
+    //   console.log(payload);
+    // });
   }, []);
 
   return (
