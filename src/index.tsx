56434e39c4af0e1ba297eee35af9f01e6eac175a
Fabio <offabio@outlook.com>
2021-01-20 11:13:31 +0100

    chore: fixed exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import { NativeModules } from 'react-native';
+import {
+  NativeModules,
+  Platform,
+  DeviceEventEmitter,
+  NativeEventEmitter,
+} from 'react-native';
 
-type BeamsWrapperType = {
-  multiply(a: number, b: number): Promise<number>;
-  connect(
+const { BeamsWrapper, BeamsWrapperEventHelper } = NativeModules;
+
+const iosEventListener = new NativeEventEmitter(BeamsWrapperEventHelper);
+
+export default {
+  multiply: (a: number, b: number): Promise<number> => {
+    return BeamsWrapper.multiply(a, b);
+  },
+  connect: (
     userId: string,
     token: string,
     instanceId: string,
     authUrl: string
-  ): void;
+  ) => {
+    return BeamsWrapper.connect(userId, token, instanceId, authUrl);
+  },
+  on(event: string, callback: Function) {
+    if (Platform.OS === 'ios') {
+      iosEventListener.addListener(event, (payload) => callback(payload));
+    } else {
+      DeviceEventEmitter.addListener(event, (payload) => callback(payload));
+    }
+  },
 };
-
-const { BeamsWrapper } = NativeModules;
-
-export default BeamsWrapper as BeamsWrapperType;
