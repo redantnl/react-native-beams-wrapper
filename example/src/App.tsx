b59cd5161a7d0335ad17fbe6c2b528a2f47d20ec
RedAnt <redant@RedBook-Air.local>
2021-02-05 10:37:08 +0100

    Added connection and notification handling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,8 +7,15 @@ import BeamsWrapper from 'react-native-beams-wrapper';
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
+  const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC93d3cuY29wYWFuLm5sIiwiYXVkIjoiaHR0cDpcL1wvd3d3LmNvcGFhbi5ubCIsImlhdCI6MTYxMjUxNjkyMywiZGF0YSI6eyJpZCI6NjUsImVtYWlsIjoibWFrZWxhYXJAcHJvbG9jby5ubCIsInJvbGUiOiJhZ2VudCIsInR5cGUiOiJub3JtYWwiLCJyZWdpb24iOjQsImFnZW50IjoxLCJzdGF0dXMiOiJhY3RpZWYifSwiZXhwIjoxNjEyNTE4NzIzfQ.bU-IYM05sGpBVNAPOgtB_BVqnmUx3nfcbriZdbIalOw";
+  const authUrl = "http://192.168.2.14/Redant/proloco-api/public/index.php/app/auth/pusher"
+  const instanceId = "4f758dae-7fb9-4314-8341-ff82ccd2b731";
+
   React.useEffect(() => {
-    // BeamsWrapper.multiply(3, 7).then(setResult);
+    BeamsWrapper.multiply(3, 7).then(setResult);
+
+    BeamsWrapper.connect('65', accessToken, instanceId, authUrl)
+
     BeamsWrapper.on('notification', function(payload) {
       console.log(payload);
     });
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,31 @@
-import { NativeModules, Platform, DeviceEventEmitter, NativeEventEmitter } from 'react-native';
+import {
+  NativeModules,
+  Platform,
+  DeviceEventEmitter,
+  NativeEventEmitter,
+} from 'react-native';
 
 const { BeamsWrapper, BeamsWrapperEventHelper } = NativeModules;
 
-const iosEventListener = new NativeEventEmitter(
-  BeamsWrapperEventHelper
-);
-
-type BeamsWrapperType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-export function on(event: string, callback: any) {
-  if (Platform.OS === 'ios') {
-    iosEventListener.addListener(event, payload => callback(payload));
-  } else {
-    DeviceEventEmitter.addListener(event, payload => callback(payload));
-  }
-}
-
-BeamsWrapper as BeamsWrapperType;
+const iosEventListener = new NativeEventEmitter(BeamsWrapperEventHelper);
 
 export default {
-  on,
+  multiply: (a: number, b: number): Promise<number> => {
+    return BeamsWrapper.multiply(a, b);
+  },
+  connect: (
+    userId: string,
+    token: string,
+    instanceId: string,
+    authUrl: string
+  ) => {
+    return BeamsWrapper.connect(userId, token, instanceId, authUrl);
+  },
+  on(event: string, callback: Function) {
+    if (Platform.OS === 'ios') {
+      iosEventListener.addListener(event, (payload) => callback(payload));
+    } else {
+      DeviceEventEmitter.addListener(event, (payload) => callback(payload));
+    }
+  },
 };
